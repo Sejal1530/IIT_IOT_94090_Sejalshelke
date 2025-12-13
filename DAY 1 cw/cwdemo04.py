1526e3112e1e025473f324ae679f1d1136ee63a0
Sejal Shelke <23uet110@dkte.ac.in>
2025-12-13 13:30:19 +0530

    DAY 1

--- DAY 1 cw/cwdemo04.py
+++
@@ -1,6 +0,0 @@
-# take length and breadth of reactangle from user and find its area
-len = int(input("Enter length : "))
-br = int(input("Enter breadth : "))
-
-area = len * br
-print(f"Area of reactangle = {area}");
